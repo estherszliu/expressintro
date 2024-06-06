463663268d6489224abd46f79d8523fc8a2a7abe
Esther Dennis <148415275+estherszliu@users.noreply.github.com>
2024-06-06 20:49:39 +1000

    feat:auth and validation

--- src/app.js
+++ src/app.js
@@ -2,17 +2,15 @@
 
 const express = require("express");
 const serverInstance = express();
-
+const {body, validationResult} = require("express-validator");
 
 // Raw JSON in body allowed
 serverInstance.use(express.json());
 // Form data in body allowed
 serverInstance.use(express.urlencoded({extended: true}));
 
-const {readAuthData, verifyAuthData} = require("./middleware/authentication.js");
 
-serverInstance.use(readAuthData);
-serverInstance.use(verifyAuthData);
+
 
 // Every route that begins with /pokemon gets passed to PokemonRouter
 const PokemonRouter = require("./routers/pokemonRoutes.js");
@@ -35,14 +33,26 @@ serverInstance.get("/", (request, response) => {
 	});
 });
 
-serverInstance.post("/", (request, response) => {
+serverInstance.post(
+	"/",  //path
+	body("username").notEmpty().isLength({min: 4, max: 10}),  // middleware in route chain 
+	(request, response) => {  // final stop in route chain 
 
-	console.log(request.body);
+		const errors = validationResult(request);
+		if (!errors.isEmpty()){
+			response.status(400).json({
+				message:"Bad username!",
+				errors: errors.array()
+			});
+		}
 
-	response.json({
-		message:"Received data:",
-		requestData: request.body
-	})
+
+		console.log(request.body);
+
+		response.json({
+			message:"Received data:",
+			requestData: request.body
+		})
 });
 
 serverInstance.put("/", (request, response) => {
